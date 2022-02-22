71f319453ae9a1e05ece7c26a6633e7645082486
EasyLanding <90074687+EasyLanding@users.noreply.github.com>
2022-02-22 23:25:00 +0300

    add js

---
+++ slider.js
@@ -0,0 +1,22 @@
+let offset = 0;
+const sliderLine = document.querySelector('.slider-line');
+
+document.querySelector('.slider-next').addEventListener('click', function ()
+{
+    offset = offset + 698;
+    if (offset > 2094)
+    {
+        offset = 0;
+    }
+    sliderLine.style.left = -offset + 'px';
+});
+
+document.querySelector('.slider-prev').addEventListener('click', function ()
+{
+    offset = offset - 698;
+    if (offset < 0)
+    {
+        offset = 2094;
+    }
+    sliderLine.style.left = -offset + 'px';
+});
